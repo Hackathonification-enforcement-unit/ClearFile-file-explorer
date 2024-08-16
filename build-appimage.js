e16838857bfc9fa68f65be5b2a4cd3790ac2a6d0
Stef-00012 <stefano.delprete.08@gmail.com>
2024-08-16 15:32:08 +0200

    idk what i changed now

--- build-appimage.js
+++ build-appimage.js
@@ -4,7 +4,7 @@ const path = require('node:path');
 const bundler = new Bundler({
     desktop: {
         name: 'ClearFile file explorer',
-        icon: path.join(__dirname, 'resources/icons/appIcon.png'),
+        icon: path.join(__dirname, 'resources/icons/appImageIcon.png'),
         categories: ['FileManager']
     },
     binary: {
